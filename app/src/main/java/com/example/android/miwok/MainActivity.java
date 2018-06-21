29f7837f387652a42af16387e97a7898986f7133
Ivan <ivan_921225@outlook.com>
2018-06-21 12:01:45 -0500

    Segundo commit con los 4 activity y un ArrayAdapter

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -15,15 +15,65 @@
  */
 package com.example.android.miwok;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView colors;
+    private TextView family;
+    private TextView numbers;
+    private TextView phrases;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
+
+        colors = (TextView) findViewById(R.id.colors);
+        family = (TextView) findViewById(R.id.family);
+        numbers = (TextView) findViewById(R.id.numbers);
+        phrases = (TextView) findViewById(R.id.phrases);
+
+        colors.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this,ColorsActivity.class);
+                startActivity(i);
+            }
+        });
+
+        family.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this,FamilyActivity.class);
+                startActivity(i);
+            }
+        });
+
+        numbers.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this,NumbersActivity.class);
+                startActivity(i);
+            }
+        });
+
+        phrases.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this,PhrasesActivity.class);
+                startActivity(i);
+            }
+        });
+
+
     }
+
+
+
 }
